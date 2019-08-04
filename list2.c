0510b4e8d91bf076845237ccf28a668f3b1daa16
Patrik Eriksson <pateriks@kth.se>
2019-08-04 17:07:59 +0200

    first commit

---
+++ list2.c
@@ -0,0 +1,197 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define NUMBER_EVENTS 100
+#define MAX_RAND 1000
+
+typedef struct eventlist{
+  double prio;
+  struct eventlist* prev;
+  struct eventlist* next;
+}eventlist;
+
+int allocated_memory = 0;
+
+//struct eventlist *list = NULL;
+struct eventlist *firstNode = NULL;
+struct eventlist *lastNode = NULL;
+
+struct eventlist* construct(double p){
+  struct eventlist* new_element = (eventlist*) malloc(sizeof(eventlist));
+  new_element->prio = p;
+  new_element->prev = NULL;
+  new_element->next = NULL;
+  return new_element;
+}
+
+
+void insertBack(struct eventlist *node){
+  struct eventlist *tmp = lastNode;
+  double p = node->prio;
+  while((tmp != NULL) && (p < tmp->prio)){
+    tmp = tmp->prev;
+  }
+  if(tmp == NULL){ //DÅ ÄR NODE FÖRSTA ELEMENTET
+    firstNode->prev = node;
+    node->next = firstNode;
+    node->prev = NULL;
+    firstNode = node;
+    return;
+  }else if(tmp == lastNode){ //DÅ ÄR NODE SISTA ELEMENTET
+    lastNode->next = node;
+    node->prev = lastNode;
+    node->next = NULL;
+    lastNode = node;
+    return;
+  }else{ //ANNARS ÄR DET NÅNSTANS I MITTEN
+    struct eventlist *tmp_next = tmp->next;
+    node->next = tmp_next;
+    node->prev = tmp;
+    tmp_next->prev = node;
+    tmp->next = node;
+    return;
+  }
+}
+
+void insertFront(struct eventlist *node){
+  struct eventlist *tmp = firstNode;
+  double p = node->prio;
+  while((tmp != NULL) && (p >= tmp->prio)){
+    tmp = tmp->next;
+  }
+  if(tmp == NULL){ //DÅ ÄR NODE SISTA ELEMENTET
+    lastNode->next = node;
+    node->prev = lastNode;
+    node->next = NULL;
+    lastNode = node;
+    return;
+  }else if(tmp == firstNode){ //DÅ ÄR NODE FÖRSTA ELEMENTET
+    node->next = firstNode;
+    firstNode->prev = node;
+    node->prev = NULL;
+    firstNode = node;
+    return;
+  }else{ //ANNARS ÄR DET NÅNSTANS I MITTEN
+    struct eventlist *tmp_prev = tmp->prev;
+    tmp_prev->next = node;
+    tmp->prev = node;
+    node->next = tmp;
+    node->prev = tmp_prev;
+    return;
+  }
+
+}
+
+void insert(double p){
+  struct eventlist *element = construct(p);
+  if((firstNode == NULL) && (lastNode == NULL)){ //FINNS INGET ELEMENT I LISTAN
+    firstNode = element;
+    lastNode = element;
+    return;
+  }
+  if(firstNode == lastNode){ //FINNS BARA ETT ELEMENT I LISTAN
+    if(p < firstNode->prio){
+      element->next = lastNode;
+      lastNode->prev = element;
+      firstNode = element;
+      return;
+    }else{
+      element->prev = firstNode;
+      firstNode->next = element;
+      lastNode = element;
+      return;
+    }
+  }
+  double firstprio = firstNode->prio;
+  double lastprio = lastNode->prio;
+  if(p > ((lastprio + firstprio) / 2)){ //FINNS MINST TVÅ ELEMENT I LISTAN
+    insertFront(element);
+  }else{
+    insertBack(element);
+  }
+}
+
+//delete tar bort elementet längst fram och skapar NUMBER_EVENTS nya events med random prioritet
+void delete(){
+  //time_t t;
+  //srand((unsigned) time(&t));
+  if(firstNode != NULL){
+    struct eventlist *delElem = firstNode;
+    double t = delElem->prio;
+    firstNode = firstNode->next;
+    if(firstNode != NULL){
+      firstNode->prev = NULL;
+    }
+    free(delElem);
+    //allocated_memory = allocated_memory - (int) sizeof(eventlist);
+    //int events = rand() % NUMBER_EVENTS;
+    for(int i = 0; i < NUMBER_EVENTS; i++){
+      double p = t + (rand() % MAX_RAND);
+      insert(p);
+    }
+  }else{
+    printf("CANNOT DELETE FROM EMPTY QUEUE! ");
+  }
+}
+
+void display_list(){
+  struct eventlist *tmp = firstNode;
+  while(tmp != NULL){
+    printf(" %lf,", tmp->prio);
+    tmp = tmp->next;
+  }
+  printf("\n");
+}
+
+//DRIVER PROGRAM
+int main(int argc, char *argv[]){
+	struct timespec t_start, t_stop;
+	clock_gettime(CLOCK_MONOTONIC_COARSE, &t_start);
+
+	int input = argv[1] != NULL ? atoi(argv[1]) : 0;
+	int input2 = argv[2] != NULL ? atoi(argv[2]) : 0;
+	if (input2 == -1) {
+		int inc = 1;
+		//best case
+		for (int i = 0; i < input; i++) {
+			insert(inc);
+		}
+	}
+	else if (input2 == -2) {
+		//worst case
+		int inc = 1;
+		int shift = 1;
+		for (int i = 0; i < input - 2; i++) {
+			if (shift == -1) {
+				insert(input - inc * shift);
+				if (input > input - 10) {
+					//printf("input = %ld\n", input + inc * shift);
+				}
+				shift = shift * -1;
+			}
+			else {
+				insert(inc);
+				if (input > input - 10) {
+					//printf("input = %ld\n", inc);
+				}
+				shift = shift * -1;
+				inc++;
+			}
+		}
+	}
+	else {
+		insert(1);
+		insert(1);
+		insert(1);
+		for (int i = 0; i < input; i++) {
+			delete();
+		}
+	}
+	clock_gettime(CLOCK_MONOTONIC_COARSE, &t_stop);
+	long wall_sec = t_stop.tv_sec - t_start.tv_sec;
+	long wall_nsec = t_stop.tv_nsec - t_start.tv_nsec;
+	long wall_msec = (wall_sec * 1000) + (wall_nsec / 1000000);
+	printf("%ld", wall_msec);
+	return 0;
+}
---
+++ skew2.c
@@ -0,0 +1,152 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <sys/time.h>
+#include <sys/resource.h>
+
+#define NUMBER_EVENTS 100
+#define MAX_RAND 1000
+
+typedef struct s_heap{
+  struct s_heap* right;
+  struct s_heap* left;
+  double priority;
+}s_heap;
+
+void swap(s_heap* a, s_heap* b);
+
+struct s_heap* root = NULL;
+
+struct s_heap *construct(double p){
+	struct s_heap *new_element = (s_heap*) malloc(sizeof(s_heap));
+	new_element->priority = p;
+	new_element->left = NULL;
+	new_element->right = NULL;
+	return new_element;
+}
+
+//swap two heaps
+void swap(s_heap* a, s_heap* b) {
+	s_heap* temp;
+	temp = a;
+	a = b;
+	b = temp;
+}
+
+//merge two heaps and returns pointer to top element
+struct s_heap *merge(s_heap *first, s_heap *second){
+	if (first == NULL) {
+		return second;
+	}else if (second == NULL) {
+		return first;
+	}else {
+		if ((first->priority) > (second->priority)) {
+			swap(first, second);
+		}
+		//self adjusting
+		swap(first->left, first->right);
+
+		first->left = merge(first->left, second);
+
+		return first;
+	}
+}
+
+//-------------------------------------------------
+void insert(double p){
+  struct s_heap* element = construct(p);
+  root = merge(root, element);
+}
+
+//delete highest priority element
+void delete(){
+  if(root != NULL){
+    struct s_heap *tmp = root;
+    tmp = merge(tmp->left, tmp->right);
+	free(root);
+	root = tmp;
+    double t = tmp->priority;
+
+    for (int i = 0; i < NUMBER_EVENTS; i++) {
+      double p = t + (rand() % MAX_RAND);
+      insert(p);
+    }
+  }else{
+    printf("CANNOT DELETE FROM EMPTY QUEUE");
+  }
+}
+//----------------------------------------------------
+
+void display_skew(s_heap *r){
+  if(r != NULL){
+    display_skew(r->left);
+    printf("%lf  ", r->priority);
+    display_skew(r->right);
+  }
+}
+
+//DRIVER PROGRAM
+int main(int argc, char *argv[]){
+	struct timespec t_start, t_stop;
+	clock_gettime(CLOCK_MONOTONIC_COARSE, &t_start);
+
+	int input = argv[1] != NULL ? atoi(argv[1]) : 0;
+	int input2 = argv[2] != NULL ? atoi(argv[2]) : 0;
+
+	if (input2 == -1) {
+		int inc = 1;
+		//best case
+		for (int i = 0; i < input; i++) {
+			insert(inc);
+			inc++;
+		}
+	}
+
+	else if (input2 == -2) {
+		//worst case
+		int inc = input;
+		for (int i = input-1; i >= 0; i--) {
+			insert(inc);
+			inc--;
+		}
+	}
+
+	else if (input2 == -3) {
+		//worst case
+		int inc = 1;
+		int shift = 1;
+		for (int i = 0; i < input - 2; i++) {
+			if (shift == -1) {
+				insert(input - inc * shift);
+				if (input > input - 10) {
+					//printf("input = %ld\n", input + inc * shift);
+				}
+				shift = shift * -1;
+			}
+			else {
+				insert(inc);
+				if (input > input - 10) {
+					//printf("input = %ld\n", inc);
+				}
+				shift = shift * -1;
+				inc++;
+			}
+		}
+	}
+
+	else {
+		insert(1);
+		insert(1);
+		insert(1);
+		for (int i = 0; i < input; i++) {
+			delete();
+		}
+	}
+
+	clock_gettime(CLOCK_MONOTONIC_COARSE, &t_stop);
+	long wall_sec = t_stop.tv_sec - t_start.tv_sec;
+	long wall_nsec = t_stop.tv_nsec - t_start.tv_nsec;
+	long wall_msec = (wall_sec * 1000) + (wall_nsec / 1000000);
+	printf("%ld", wall_msec);
+	return 0;
+}
